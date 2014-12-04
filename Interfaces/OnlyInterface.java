913e7913717c2c390c1aea8c668ff3ce80109098
Mykola Bova <bova.mykola@gmail.com>
2014-12-04 12:40:01 +0200

    1

---
+++ Interfaces/OnlyInterface.java
@@ -0,0 +1,12 @@
+public class OnlyInterface {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+
+	//MyInterface mi = new MyInterface(){};	
+
+	MyInterfaceDummy di = new MyInterfaceDummy(){};
+
+    }
+
+}
